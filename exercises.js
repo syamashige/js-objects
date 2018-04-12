566edb3239c69833b390f186605c9de74c6837d3
Sarah <yamashige.sarah@gmail.com>
2018-04-11 21:53:32 -1000

    Finished exercise 7.

--- exercises.js
+++ exercises.js
@@ -43,10 +43,14 @@ var dog = {
     age: 12,
     vegetarian: false,
     color: ["black", "brown"],
-    speak: "bark!"
+    //speak: "bark!"
+    speak: function () {
+        return "Bark!"
+    }
 };
 
-console.log("My dog " + dog.name + " is " + dog.age + " year old and likes to " + dog.speak + " at strangers.");
+console.log(dog);
+console.log("My dog " + dog.name + " is " + dog.age + " year old and likes to " + dog.speak() + " at strangers.");
 
 
 //An empty object
@@ -70,7 +74,10 @@ kicks.brand = "InsertSneakerBrandHere";
 kicks.color = "cornflower blue";
 kicks.size = 5;
 kicks.price = 55;
-kicks.buy = "Now I is a cook kid!";
+//kicks.buy = "Now I is a cook kid!";
+kicks.buy = function () {
+    return "Now I is a cool kid!";
+};
 
 console.log(kicks);
 
@@ -173,6 +180,70 @@ console.log(stockCar);
             ...
  */
 
+ var arrayOfObjects = [
+    {
+      id: 0,
+      date: "Monday Jan 25 2015 2:01 PM",
+      total: "279.38"
+    },
+    {
+      id: 1,
+      date: "Monday Jan 27 2015 11:31 AM",
+      total: "79.80"
+    },
+    {
+      id: 2,
+      date: "Monday Feb 1 2015 7:56 AM",
+      total: "15.62"
+    },
+    {
+      id: 3,
+      date: "Monday Feb 1 2015 9:43 AM",
+      total: "19.83"
+    },
+    {
+      id: 4,
+      date: "Monday Feb 1 2015 11:08 PM",
+      total: "56.69"
+    },
+    {
+      id: 5,
+      date: "Monday Feb 13 2015 10:22 AM",
+      total: "137.92"
+    },
+    {
+      id: 6,
+      date: "Monday Feb 14 2015 6:54 PM",
+      total: "938.65"
+    },
+    {
+      id: 7,
+      date: "Monday Feb 14 2015 7:17 PM",
+      total: "43.77"
+    },
+    {
+      id: 8,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "28.54"
+    },
+    {
+      id: 9,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "194.33"
+    }
+  ]
+
+  function printOrders (orders) {
+      for (var i = 0; i < orders.length; i++) {
+         console.log("=====");
+         console.log("id: " + orders[i].id);
+         console.log("date: " + orders[i].date);
+         console.log("total: " + orders[i].total);
+      }
+  }
+  printOrders(arrayOfObjects);
+
+
 
 /*
 8. Addition with an object
@@ -186,6 +257,7 @@ console.log(stockCar);
         Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log` 
         to inspect your results.
 */
+
 
 
 /*
